6b468038aead9c215f6250eea546eee1511c96f5
David Crawshaw <crawshaw@google.com>
2011-09-18 20:51:47 -0700

    graphics: gaussian blur
    R=nigeltao
    CC=golang-dev
    http://codereview.appspot.com/5012047

---
+++ graphics/blur.go
@@ -0,0 +1,189 @@
+// Copyright 2011 The Graphics-Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package graphics
+
+import (
+	"image"
+	"image/draw"
+	"math"
+)
+
+func circularGauss(x float64, sd float64) float64 {
+	sdSq := sd * sd
+	// If we were calculating the standard 1D Gaussian, this would
+	// be 1/math.Sqrt(...). However this is a single dimension of
+	// the circularly symmetric Gaussian. So no Sqrt.
+	coeff := 1 / (2 * math.Pi * sdSq)
+	return coeff * math.Pow(math.E, -1*math.Pow(x, 2)/(2*sdSq))
+}
+
+func blurRGBA(src *image.RGBA, sd float64, size int) *image.RGBA {
+	kernel := make([]float64, size+1)
+	for i := 0; i <= size; i++ {
+		kernel[i] = circularGauss(float64(i), sd)
+	}
+
+	// TODO(crawshaw): Why don't we add kernel[0] here?
+	kernelSum := 0.0
+	for i := 1; i <= size; i++ {
+		kernelSum += kernel[i]
+	}
+	kernelSum *= 2
+
+	bounds := src.Bounds()
+	width := bounds.Max.X - bounds.Min.X
+	height := bounds.Max.Y - bounds.Min.Y
+	ds1 := make([]float64, width*height*4)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			var r, g, b, a float64
+			off := (y-src.Rect.Min.Y)*src.Stride + (x-src.Rect.Min.X)*4
+
+			// When we fail to copy non-existant data near the boundaries of the
+			// image, maintain an adjustment factor and use it to take more from
+			// the original pixel. Avoids vignetting.
+			adjust := float64(0)
+
+			// Add blur from above.
+			for i := 1; i <= size; i++ {
+				if y-i < bounds.Min.Y {
+					adjust += kernel[i]
+				} else {
+					o := off - i*src.Stride
+					factor := kernel[i]
+					r += float64(src.Pix[o+0]) * factor
+					g += float64(src.Pix[o+1]) * factor
+					b += float64(src.Pix[o+2]) * factor
+					a += float64(src.Pix[o+3]) * factor
+				}
+			}
+
+			// Add blur from below.
+			for i := 1; i <= size; i++ {
+				if y+i >= bounds.Max.Y {
+					adjust += kernel[i]
+				} else {
+					o := off + i*src.Stride
+					factor := kernel[i]
+					r += float64(src.Pix[o+0]) * factor
+					g += float64(src.Pix[o+1]) * factor
+					b += float64(src.Pix[o+2]) * factor
+					a += float64(src.Pix[o+3]) * factor
+				}
+			}
+
+			// Add a fraction of the original pixel.
+			factor := kernel[0] + adjust
+			r += float64(src.Pix[off+0]) * factor
+			g += float64(src.Pix[off+1]) * factor
+			b += float64(src.Pix[off+2]) * factor
+			a += float64(src.Pix[off+3]) * factor
+
+			o := (y-bounds.Min.Y)*width*4 + (x-bounds.Min.X)*4
+			ds1[o+0] = r
+			ds1[o+1] = g
+			ds1[o+2] = b
+			ds1[o+3] = a
+		}
+	}
+
+	dst := image.NewRGBA(src.Bounds())
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			var r, g, b, a float64
+			adjust := float64(0)
+			off := y*width*4 + x*4
+
+			// Add blur from the left.
+			for i := 1; i <= size; i++ {
+				if x-i < 0 {
+					adjust += kernel[i]
+				} else {
+					o := off - i*4
+					factor := kernel[i]
+					r += ds1[o+0] * factor
+					g += ds1[o+1] * factor
+					b += ds1[o+2] * factor
+					a += ds1[o+3] * factor
+				}
+			}
+
+			// Add blur from the right.
+			for i := 1; i <= size; i++ {
+				if x+i >= width {
+					adjust += kernel[i]
+				} else {
+					o := off + i*4
+					factor := kernel[i]
+					r += ds1[o+0] * factor
+					g += ds1[o+1] * factor
+					b += ds1[o+2] * factor
+					a += ds1[o+3] * factor
+				}
+			}
+
+			// Add a fraction of the original pixel.
+			factor := kernel[0] + adjust
+			r += ds1[off+0] * factor
+			g += ds1[off+1] * factor
+			b += ds1[off+2] * factor
+			a += ds1[off+3] * factor
+
+			// When the kernel does not sum to 1.0, the resulting image has
+			// less intensity than the original. Dividing through by the sum
+			// corrects this.
+			r /= kernelSum
+			g /= kernelSum
+			b /= kernelSum
+			a /= kernelSum
+
+			dstOff := (y-dst.Rect.Min.Y)*dst.Stride + (x-dst.Rect.Min.X)*4
+			dst.Pix[dstOff+0] = uint8(math.Fmin(255.0, r))
+			dst.Pix[dstOff+1] = uint8(math.Fmin(255.0, g))
+			dst.Pix[dstOff+2] = uint8(math.Fmin(255.0, b))
+			dst.Pix[dstOff+3] = uint8(math.Fmin(255.0, a))
+		}
+	}
+
+	return dst
+}
+
+// DefaultStdDev is the default blurring parameter.
+var DefaultStdDev = 0.5
+
+// BlurOptions are the blurring parameters.
+// StdDev is the standard deviation of the normal, higher is blurrier.
+// Size is the size of the kernel. If zero, it is set to Ceil(6 * StdDev).
+type BlurOptions struct {
+	StdDev float64
+	Size   int
+}
+
+// Blur produces a blurred version of the image, using a Gaussian blur.
+func Blur(src image.Image, opt *BlurOptions) image.Image {
+	sd := DefaultStdDev
+	size := 0
+
+	if opt != nil {
+		sd = opt.StdDev
+		size = opt.Size
+	}
+
+	if size == 0 {
+		size = int(math.Ceil(sd * 6))
+	}
+
+	rgba, ok := src.(*image.RGBA)
+	if !ok {
+		// Make a copy of src as an image.RGBA.
+		b := src.Bounds()
+		rgba = image.NewRGBA(b)
+		draw.Draw(rgba, b, src, b.Min, draw.Src)
+	}
+
+	return blurRGBA(rgba, sd, size)
+}
---
+++ graphics/blur_test.go
@@ -0,0 +1,252 @@
+// Copyright 2011 The Graphics-Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package graphics_test
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"os"
+	"testing"
+
+	"graphics-go.googlecode.com/hg/graphics"
+)
+
+func delta(u0, u1 uint32) int {
+	d := int(u0) - int(u1)
+	if d < 0 {
+		return -d
+	}
+	return d
+}
+
+func withinTolerance(c0, c1 image.Color, tolerance int) bool {
+	r0, g0, b0, a0 := c0.RGBA()
+	r1, g1, b1, a1 := c1.RGBA()
+	r := delta(r0, r1)
+	g := delta(g0, g1)
+	b := delta(b0, b1)
+	a := delta(a0, a1)
+	return r <= tolerance && g <= tolerance && b <= tolerance && a <= tolerance
+}
+
+func sprintBox(box []byte, width, height int) string {
+	buf := bytes.NewBuffer(nil)
+	i := 0
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			fmt.Fprintf(buf, " 0x%02x,", box[i])
+			i++
+		}
+		buf.WriteByte('\n')
+	}
+	return buf.String()
+}
+
+type blurOneColorTest struct {
+	desc   string
+	width  int
+	height int
+	opt    *graphics.BlurOptions
+	src    []uint8
+	res    []uint8
+}
+
+var blurOneColorTests = []blurOneColorTest{
+	{
+		"1x1-blank", 1, 1,
+		&graphics.BlurOptions{0.83, 1},
+		[]uint8{0xff},
+		[]uint8{0xeb},
+	},
+	{
+		"1x1-spreadblank", 1, 1,
+		&graphics.BlurOptions{0.83, 2},
+		[]uint8{0xff},
+		[]uint8{0xeb},
+	},
+	{
+		"3x3-blank", 3, 3,
+		&graphics.BlurOptions{0.83, 2},
+		[]uint8{
+			0xff, 0xff, 0xff,
+			0xff, 0xff, 0xff,
+			0xff, 0xff, 0xff,
+		},
+		[]uint8{
+			0xeb, 0xeb, 0xeb,
+			0xeb, 0xeb, 0xeb,
+			0xeb, 0xeb, 0xeb,
+		},
+	},
+	{
+		"3x3-dot", 3, 3,
+		&graphics.BlurOptions{0.34, 1},
+		[]uint8{
+			0x00, 0x00, 0x00,
+			0x00, 0xff, 0x00,
+			0x00, 0x00, 0x00,
+		},
+		[]uint8{
+			0x02, 0xaf, 0x02,
+			0xaf, 0xff, 0xaf,
+			0x02, 0xaf, 0x02,
+		},
+	},
+	{
+		"5x5-dot", 5, 5,
+		&graphics.BlurOptions{0.34, 1},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0xff, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x02, 0xaf, 0x02, 0x00,
+			0x00, 0xaf, 0xff, 0xaf, 0x00,
+			0x00, 0x02, 0xaf, 0x02, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+	},
+	{
+		"5x5-dot-spread", 5, 5,
+		&graphics.BlurOptions{0.85, 1},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0xff, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x0e, 0x1c, 0x0e, 0x00,
+			0x00, 0x1c, 0x38, 0x1c, 0x00,
+			0x00, 0x0e, 0x1c, 0x0e, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+	},
+	{
+		"4x4-box", 4, 4,
+		&graphics.BlurOptions{0.34, 1},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0xff, 0xff, 0x00,
+			0x00, 0xff, 0xff, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+		},
+		[]uint8{
+			0x02, 0xb1, 0xb1, 0x02,
+			0xb1, 0xff, 0xff, 0xb1,
+			0xb1, 0xff, 0xff, 0xb1,
+			0x02, 0xb1, 0xb1, 0x02,
+		},
+	},
+	{
+		"5x5-twodots", 5, 5,
+		&graphics.BlurOptions{0.34, 1},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x96, 0x00, 0x96, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+		[]uint8{
+			0x00, 0x00, 0x00, 0x00, 0x00,
+			0x01, 0x67, 0x02, 0x67, 0x01,
+			0x67, 0xff, 0xce, 0xff, 0x67,
+			0x01, 0x67, 0x02, 0x67, 0x01,
+			0x00, 0x00, 0x00, 0x00, 0x00,
+		},
+	},
+}
+
+func TestBlurOneColor(t *testing.T) {
+	for _, oc := range blurOneColorTests {
+		img := image.NewRGBA(image.Rect(0, 0, oc.width, oc.height))
+		i := 0
+		for y := 0; y < oc.height; y++ {
+			for x := 0; x < oc.width; x++ {
+				img.SetRGBA(x, y, image.RGBAColor{R: oc.src[i]})
+				i++
+			}
+		}
+
+		imgBlur, ok := graphics.Blur(img, oc.opt).(*image.RGBA)
+		if !ok {
+			t.Errorf("%s: blurred image is not RGBA", oc.desc)
+			continue
+		}
+
+		i = 0
+		res := make([]byte, len(oc.res))
+		for y := 0; y < oc.height; y++ {
+			for x := 0; x < oc.width; x++ {
+				off := (y-img.Rect.Min.Y)*img.Stride + (x-img.Rect.Min.X)*4
+				res[i] = imgBlur.Pix[off]
+				i++
+			}
+		}
+
+		if !bytes.Equal(res, oc.res) {
+			got := sprintBox(res, oc.width, oc.height)
+			want := sprintBox(oc.res, oc.width, oc.height)
+			t.Errorf("%s:\n got\n%s\n want\n%s", oc.desc, got, want)
+			continue
+		}
+	}
+}
+
+func benchBlur(b *testing.B, bounds image.Rectangle) {
+	b.StopTimer()
+
+	// Construct a fuzzy image.
+	img := image.NewRGBA(bounds)
+	dy := bounds.Dy()
+	dx := bounds.Dx()
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx/4; x++ {
+			img.SetRGBA(x, y, image.RGBAColor{
+				uint8(5 * x % 0x100),
+				uint8(7 * y % 0x100),
+				uint8((7*x + 5*y) % 0x100),
+				0xff,
+			})
+		}
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		graphics.Blur(img, &graphics.BlurOptions{0.84, 3})
+	}
+}
+
+func loadImage(path string) (img image.Image, err os.Error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	img, _, err = image.Decode(file)
+	return
+}
+
+func BenchmarkBlur400x400x3(b *testing.B) {
+	benchBlur(b, image.Rect(0, 0, 400, 400))
+}
+
+// Exactly twice the pixel count of 400x400.
+func BenchmarkBlur400x800x3(b *testing.B) {
+	benchBlur(b, image.Rect(0, 0, 400, 800))
+}
+
+// Exactly twice the pixel count of 400x800
+func BenchmarkBlur400x1600x3(b *testing.B) {
+	benchBlur(b, image.Rect(0, 0, 400, 1600))
+}
