4a11f0a26d1340c84d52e7619906d2a0e694d406
David Crawshaw <crawshaw@google.com>
2011-10-04 21:52:10 -0700

    graphics: add Thumbnail function
    R=nigeltao
    CC=golang-dev
    http://codereview.appspot.com/5169044

---
+++ graphics/thumbnail_test.go
@@ -0,0 +1,52 @@
+// Copyright 2011 The Graphics-Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package graphics
+
+import (
+	"image"
+	"testing"
+
+	_ "image/png"
+)
+
+func TestThumbnailGopher(t *testing.T) {
+	dst := image.NewRGBA(image.Rect(0, 0, 80, 80))
+
+	src, err := loadImage("../testdata/gopher.png")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	Thumbnail(dst, src)
+	cmp, err := loadImage("../testdata/gopher-thumb-80x80.png")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = imageWithinTolerance(dst, cmp, 0)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestThumbnailLongGopher(t *testing.T) {
+	dst := image.NewRGBA(image.Rect(0, 0, 50, 150))
+
+	src, err := loadImage("../testdata/gopher.png")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	Thumbnail(dst, src)
+	cmp, err := loadImage("../testdata/gopher-thumb-50x150.png")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = imageWithinTolerance(dst, cmp, 0)
+	if err != nil {
+		t.Error(err)
+	}
+}
